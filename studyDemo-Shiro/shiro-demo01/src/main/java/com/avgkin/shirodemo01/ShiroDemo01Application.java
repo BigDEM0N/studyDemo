a1fb8897299831f18086c18a2588e4844fdf12fd
BigDEM0N <2678757810@qq.com>
2023-10-31 17:54:23 +0800

    demo01started

---
+++ studyDemo-Shiro/shiro-demo01/src/main/java/com/avgkin/shirodemo01/ShiroDemo01Application.java
@@ -0,0 +1,13 @@
+package com.avgkin.shirodemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroDemo01Application.class, args);
+    }
+
+}
---
+++ studyDemo-Shiro/shiro-demo01/src/test/java/com/avgkin/shirodemo01/ShiroDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avgkin.shirodemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
