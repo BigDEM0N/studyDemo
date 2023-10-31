071b1ebf323fd6fab02d24cdfbc0c697401626ca
BigDEM0N <2678757810@qq.com>
2023-10-31 15:55:46 +0800

    init

---
+++ studyDemo-Dubbo/studyDemo-zookeeper/src/main/java/org/example/ZookeeperApi.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+public class ZookeeperApi {
+    public static void main(String[] args) throws IOException {
+        try {
+            final CountDownLatch countDownLatch=new CountDownLatch(1);
+            ZooKeeper zooKeeper=
+                    new ZooKeeper("localhost:2181",
+                            4000, new Watcher() {
+                        @Override
+                        public void process(WatchedEvent event) {
+                            if(Event.KeeperState.SyncConnected==event.getState()){
+                                //如果收到了服务端的响应事件，连接成功
+                                countDownLatch.countDown();
+                            }
+                        }
+                    });
+            countDownLatch.await();
+            //CONNECTED
+            System.out.println(zooKeeper.getState());
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
